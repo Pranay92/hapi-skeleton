31fb0621bf01ca22c8cfee7c0d9315b799fcba69
Pranay Dubey <pranaydubey3009@gmail.com>
2016-03-02 18:09:00 +0530

    Updates on boilerplate  - Add auth module  - Add app bootstrap feature  - Add database startup

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ var hapi = require('hapi'),
 server = module.exports = new hapi.Server();        
 server.connection({'port' : process.env.NODE_PORT || 3000});
 
+require('database');
+
+require('modules');
+
+require('bootstrap');
+
 server.start(function() {
   console.log('Server is up and running');
 });
